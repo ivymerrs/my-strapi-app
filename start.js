2475073b9bd672a5e8eeff5777986dd5d439cdcd
ivy <ivymerrs@126.com>
2025-07-29 11:55:47 +0800

    Fix start script and improve SWC disabling configuration

--- start.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// Custom start script to bypass upload plugin issues
-process.env.STRAPI_DISABLE_UPLOAD = 'true';
-
-const strapi = require('@strapi/strapi');
-
-// Start Strapi
-strapi().start(); 
